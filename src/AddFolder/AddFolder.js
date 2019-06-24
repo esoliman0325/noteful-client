bd748b7d78fcb4e2abecaef1ee106a35b1923e58
Ed Soliman <e.georgesoliman@gmail.com>
2019-06-24 16:49:04 -0700

    added addfolder

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
-import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
 import config from '../config'
 import './AddFolder.css'
+import ErrorBoundary from '../ErrorBoundaries/ErrorBoundary';
 
 export default class AddFolder extends Component {
-  static defaultProps = {
-    history: {
-      push: () => { }
-    },
+  constructor(props) {
+    super(props);
+    this.folderName = React.createRef();
   }
+  
   static contextType = ApiContext;
 
   handleSubmit = e => {
     e.preventDefault()
     const folder = {
-      name: e.target['folder-name'].value
+      name: this.folderName.current.value
     }
     fetch(`${config.API_ENDPOINT}/folders`, {
       method: 'POST',
@@ -26,7 +26,7 @@ export default class AddFolder extends Component {
     })
       .then(res => {
         if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
+          throw new Error('Something went wrong');
         return res.json()
       })
       .then(folder => {
@@ -40,21 +40,23 @@ export default class AddFolder extends Component {
 
   render() {
     return (
-      <section className='AddFolder'>
-        <h2>Create a folder</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
-          <div className='field'>
-            <label htmlFor='folder-name-input'>
-              Name
-            </label>
-            <input type='text' id='folder-name-input' name='folder-name' />
-          </div>
-          <div className='buttons'>
-            <button type='submit'>
-              Add folder
-            </button>
-          </div>
-        </NotefulForm>
+      <section className='FolderAddition'>
+        <ErrorBoundary>
+          <h2>Create a folder</h2>
+            <form onSubmit={this.handleSubmit}>
+              <div className='field'>
+                <label htmlFor='folder-name-input'>
+                  Name
+                </label>
+                <input type='text' id='folder-name-input' ref={this.folderName} />
+              </div>
+              <div className='buttons'>
+                <button type='submit'>
+                  Add folder
+                </button>
+              </div>
+            </form>
+        </ErrorBoundary>
       </section>
     )
   }
